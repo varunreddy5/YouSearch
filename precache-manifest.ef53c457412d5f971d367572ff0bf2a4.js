4ac3ee919c45974dffdfe4f4766742c1c8831d91
Varun Reddy Regalla <varunreddy1061@gmail.com>
2020-10-13 15:22:58 -0400

    Updates

---
+++ precache-manifest.ef53c457412d5f971d367572ff0bf2a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b16ad0cbcbe7988441febdc7e0cffcac",
+    "url": "/YouSearch/index.html"
+  },
+  {
+    "revision": "15ff420208f592cf03e3",
+    "url": "/YouSearch/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "62bbb7524d98e34ed90b",
+    "url": "/YouSearch/static/css/main.c98dc09d.chunk.css"
+  },
+  {
+    "revision": "15ff420208f592cf03e3",
+    "url": "/YouSearch/static/js/2.d6b17a63.chunk.js"
+  },
+  {
+    "revision": "a8eb76e4c6beb72c22bcc9f3c026930b",
+    "url": "/YouSearch/static/js/2.d6b17a63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62bbb7524d98e34ed90b",
+    "url": "/YouSearch/static/js/main.2894e59a.chunk.js"
+  },
+  {
+    "revision": "8e3b2e9a8358f2730f93",
+    "url": "/YouSearch/static/js/runtime-main.deaa13a3.js"
+  }
+]);
